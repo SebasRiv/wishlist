7f0ee719368235aa81e593f91b3524f53fea3d11
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-11-02 11:09:59 -0500

    creado reservas-api-client

--- src/app/reservas/reservas.module.ts
+++ src/app/reservas/reservas.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ReservasRoutingModule } from './reservas-routing.module';
 import { ReservasListadoComponent } from './reservas-listado/reservas-listado.component';
 import { ReservasDetalleComponent } from './reservas-detalle/reservas-detalle.component';
+import { ReservasApiClientService } from './reservas-api-client.service';
 
 
 @NgModule({
   declarations: [ReservasListadoComponent, ReservasDetalleComponent],
+  providers: [
+    ReservasApiClientService
+  ],
   imports: [
     CommonModule,
     ReservasRoutingModule
